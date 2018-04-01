ce4c167e28ba45ee470de614e37da1b66c318903
Aneurin Price <nprice@dizions.co.uk>
2018-04-01 11:37:14 +0100

    Update metadata for Workshop release

--- metadata.lua
+++ metadata.lua
@@ -1,15 +1,15 @@
 return PlaceObj('ModDef', {
 	'title', "Info Bar",
 	'description', "Adds an information bar to the top of the screen, showing you all your resources, research, and funding, plus  vacant housing/homelessnes and open jobs/unemployment.",
-	'tags', "UI, HUD, Resources, Interface",
+	'tags', "UI, HUD, Resources, Interface, English, Korean",
 	'image', "SteamPreviewImage.png",
 	'id', "wPlq2aw",
 	'steam_id', "1337627392",
 	'author', "Waywocket",
-	'version', 18,
+	'version', 21,
 	'lua_revision', 228184,
 	'code', {
 		"Code/Script.lua",
 	},
-	'saved', 1522363843,
+	'saved', 1522511936,
 })
